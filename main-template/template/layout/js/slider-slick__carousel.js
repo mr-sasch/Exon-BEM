a99a842687deb6af5a8b58ff9de0d48e6fb3e3ff
Alex Makovsky <alex.m.sky@gmail.com>
2017-10-16 11:39:57 +0300

    О компании - готово

---
+++ main-template/template/layout/js/slider-slick__carousel.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	$('.slick-slider__carousel').slick({
+	  dots: false,
+		arrows: false,
+	  speed: 300,
+	  slidesToShow: 5,
+		infinite: true,
+		responsive: [
+			{
+				breakpoint: 1300,
+				settings: {
+					slidesToShow: 4,
+				}
+			},
+			{
+				breakpoint: 1000,
+				settings: {
+					slidesToShow: 3,
+				}
+			},
+			{
+				breakpoint: 750,
+				settings: {
+					slidesToShow: 2,
+				}
+			},
+			{
+				breakpoint: 500,
+				settings: {
+					slidesToShow: 1,
+				}
+			},
+		]
+	});
+});
